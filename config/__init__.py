058a68606615faadd75f022a9c6322bc4969c860
Denis Golovchanskiy <biza3154@gmail.com>
2023-04-02 00:10:07 +0300

    fix

--- config/__init__.py
+++ config/__init__.py
@@ -3,10 +3,10 @@ from dotenv import dotenv_values
 
 
 try:
-    if os.path.exists('.venv.templates'):
-        config = dotenv_values('.venv.templates')
-    elif os.path.exists('../.venv.templates'):
-        config = dotenv_values('../.venv.templates')
+    if os.path.exists('.venv'):
+        config = dotenv_values('.venv')
+    elif os.path.exists('../.venv'):
+        config = dotenv_values('../.venv')
     else:
         raise FileExistsError('Файл ".venv.templates" отсутвует, либо находится вне корневого каталога.')
 except FileExistsError:
